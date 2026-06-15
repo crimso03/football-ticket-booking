ff6cf37bee267ea4907f6006dffa01bf099f91fa
crimso03 <sihabunsakibh1@gmail.com>
2026-06-15 17:25:28 +0600

    Queries completed

--- QUERY.sql
+++ QUERY.sql
@@ -12,3 +12,88 @@ where
 
 
 
+select
+  user_id,
+  full_name,
+  email
+from
+
+  users
+where
+  full_name ilike 'Tanvir%'
+  or full_name ilike '%Haque%';
+
+
+
+
+
+  select
+  booking_id,
+  user_id,
+  match_id,
+  coalesce(payment_status, 'Action Required') as systematic_status
+from
+
+  bookings
+where
+  payment_status is null;
+
+
+
+
+
+select
+  b.booking_id,
+  u.full_name,
+  m.fixture,
+  b.total_cost
+from
+  bookings b
+  inner join users u on b.user_id = u.user_id
+  inner join matches m on b.match_id = m.match_id;
+
+
+
+
+
+select
+  u.user_id,
+  u.full_name,
+  b.booking_id
+from
+  users u
+  left join bookings b on u.user_id = b.user_id;
+
+
+
+
+  select
+  booking_id,
+  match_id,
+  total_cost
+from
+  bookings
+where
+  total_cost > (
+    select
+      avg(total_cost)
+    from
+      bookings
+  );
+
+
+
+
+select
+  match_id,
+  fixture,
+  base_ticket_price
+from
+  matches
+  
+order by
+  base_ticket_price desc
+limit
+  2
+offset
+  1;
